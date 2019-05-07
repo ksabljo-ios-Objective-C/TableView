c2700e55d769d8d5d5ff117db30ce4ecfef38ef2
Krešimir Sabljo <ksabljo@icloud.com>
2019-05-07 10:40:42 +0200

    Initial Commit

---
+++ TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by Krešimir Sabljo on 07/05/2019.
+//  Copyright © 2019 Krešimir Sabljo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
